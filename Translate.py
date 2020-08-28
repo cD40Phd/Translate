35446d4266c160dd7b76cd608018d52a33dd8dcb
Вернер Сименс <Werner.von.Siemens@outlook.com>
2020-08-28 13:03:56 +0300

    Yes

--- Translate.py
+++ Translate.py
@@ -1,5 +1,3 @@
-# Перевод текста с помощью Google Translate API в Python
-# https://dev-gang.ru/article/perevod-teksta-s-pomosczu-google-translate-api-v-python-ahgm88wx1k/ 
 
 from googletrans import Translator
 import datetime
@@ -17,4 +15,6 @@ result = translator.translate(contents, dest='ru')
 print(result.text)
 with open('C:\\Users\\Dmitry\\Documents\\MEGA\\MEGAsync\\Translate\\Python_translate.txt', 'a+', encoding='utf8') as f:
     f.write(data_log.strftime('%d-%m-%Y') + '\n' + result.text + '\1\n\r') # добавляем перевод в файл на рабочем столе
-    
+
+with open('C:\\Users\\Dmitry\\Documents\\MEGA\\MEGAsync\\Translate\\Original_translate.txt', 'a+', encoding='utf8') as f:
+    f.write(data_log.strftime('%d-%m-%Y') + '\n' + contents + '\1\n\r') # добавляем перевод в файл на рабочем столе
